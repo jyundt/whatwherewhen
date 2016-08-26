713d010c5abcbf150d1cc02f3369dda4d57e5bfb
Jacob Yundt <jyundt@gmail.com>
2016-08-26 15:24:41 -0400

    Add email addresses to tuple and include checks for missing EXIF data.

--- lambda/email_handler.py
+++ lambda/email_handler.py
@@ -43,6 +43,11 @@ def extract_images(mailfile):
     mail = email.message_from_file(fh)
     fh.close()
     images = []
+    emailaddress = ''
+   
+    for header in mail._headers:
+        if header[0] == 'From':
+            emailaddress = header[1]
     for part in mail.walk():
         if part.get_content_maintype() == 'multipart':
             continue
@@ -55,7 +60,7 @@ def extract_images(mailfile):
             exif_info = i._getexif()
             i.close()
             fh.close()
-            if 34853 in exif_info:
+            if exif_info and all (x in exif_info[34853] for x in (1,2,3,4)):
                 gps_lat = exif_info[34853][2]
                 gps_lat_ref = exif_info[34853][1]
                 gps_lon = exif_info[34853][4]
@@ -75,7 +80,7 @@ def extract_images(mailfile):
                 filehashname = '/tmp/' + hashlib.sha256(fh.read()).hexdigest() + '.jpg'
                 fh.close()
                 os.rename('/tmp/raw.jpg',filehashname)
-                images.append((filehashname, timestamp, lat, lon))
+                images.append((filehashname, timestamp, lat, lon, emailaddress))
             
     return(images)
 def handler(event, context):
@@ -84,12 +89,16 @@ def handler(event, context):
         key = record['s3']['object']['key']
         mailfile = '/tmp/' + os.path.basename(key)
 
+        print(key)
+        print(mailfile)
         s3_client.download_file(bucket,key,mailfile)
         images = extract_images(mailfile)
+        print(images)
         for image in images:
-            s3_client.upload_file(image[0],bucket,'images/'+os.path.basename(image[0]), ExtraArgs={'ContentType': 'image/jpeg'})
+            print(image)
+            s3_client.upload_file(image[0],bucket,'images/'+os.path.basename(image[0]), ExtraArgs={'ContentType': 'image/jpeg', 'ACL': 'public-read'})
             generate_thumbnail(image[0])
-            s3_client.upload_file(image[0],bucket,'thumbnails/'+os.path.basename(image[0]), ExtraArgs={'ContentType': 'image/jpeg'})
+            s3_client.upload_file(image[0],bucket,'thumbnails/'+os.path.basename(image[0]), ExtraArgs={'ContentType': 'image/jpeg', 'ACL': 'public-read'})
 
 
 def main():
